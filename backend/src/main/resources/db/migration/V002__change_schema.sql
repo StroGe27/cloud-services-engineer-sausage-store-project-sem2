07f6c95fbe8ddd141d88f12a1e9150587efb1808
Stroganov Georgiy <stroganovgeorgi01@gmail.com>
2026-06-24 20:03:51 +0000

    fix migrations (duplicate orders_pkey)

--- backend/src/main/resources/db/migration/V002__change_schema.sql
+++ backend/src/main/resources/db/migration/V002__change_schema.sql
@@ -1,7 +1,19 @@
 ALTER TABLE product
 ADD COLUMN price double precision;
+
+UPDATE product p
+SET price = pi.price
+FROM product_info pi
+WHERE p.id = pi.product_id;
+
 ALTER TABLE orders
 ADD COLUMN date_created date;
+
+UPDATE orders o
+SET date_created = od.date_created,
+    status = COALESCE(o.status, od.status)
+FROM orders_date od
+WHERE o.id = od.order_id;
 
 ALTER TABLE product
 ADD CONSTRAINT product_pkey PRIMARY KEY (id);
